87c3e8c39ff16e57d5add599c2e0a62d29897a1a
tony <tony.aldon.adm@gmail.com>
2020-07-07 11:19:39 +0200

    Add component (Header)
    * public/index.html:  Update (title).
    * src/App.js:  Add component (Header).
    * src/App.test.js:  Add test component (Header).

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function Header() {
+	return (
+		<header>
+			<h1>Tricks by Tony Aldon</h1>
+		</header>
+	);
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Header from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders header content', () => {
+  const { getByText } = render(<Header />);
+  const headerContent = getByText(/Tricks by Tony Aldon/i);
+  expect(headerContent).toBeInTheDocument();
 });
