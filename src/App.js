412e636d16f2686b979beab1f72c945183ac4f3b
tony <tony.aldon.adm@gmail.com>
2020-07-08 19:04:46 +0200

    Add component (Trick)
    * src/App.js:  Add component (Trick).  Update component (App).
    * src/App.test.js:  Add test block (App).  Add test (renders "Trick"
    component).

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { astMakeTrick, astMakeTricks } from './astUtils.js'
 
 function Header() {
-	return (
-		<header>
-			<h1>Tricks by Tony Aldon</h1>
-		</header>
-	);
+  return (
+    <header>
+      <h1>Tricks by Tony Aldon</h1>
+    </header>
+  );
 }
 
-function App() {
-  return (
+function App(props) {
+  const ast = props.ast;
+  const tricks = astMakeTricks(ast);
+	const trickList = tricks.map((trick,index) =>
+		<Trick
+			key={index}
+			trick={trick}
+		/>);
+	return (
     <div className="App">
       <Header />
+      <ul>{trickList}</ul>
     </div>
   );
 }
 
-export default App;
+function Trick(props) {
+  return (
+    <div>
+      <p>{props.trick.headline_3}</p>
+    </div>
+  );
+}
+
+export { App, Header, Trick };
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,73 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
-import Header from './App';
+import { screen } from '@testing-library/dom'
+import { App, Header, Trick } from './App';
+import { parse } from 'orga';
+import { astMakeTrick } from './astUtils.js'
 
-test('renders header content', () => {
+it('renders header content', () => {
   const { getByText } = render(<Header />);
   const headerContent = getByText(/Tricks by Tony Aldon/i);
   expect(headerContent).toBeInTheDocument();
 });
+
+it('renders "Trick" component', () => {
+  const content = `
+* Headline 1
+** Headline 1.1
+*** Headline 1.1.1
+- item 1
+- item 2
+#+BEGIN_SRC bash
+some code 1
+#+END_SRC
+`;
+
+  const ast = parse(content);
+  const astTopSection = ast.children[0];
+  const trickNumber = 1;
+  const trick = astMakeTrick(astTopSection, trickNumber);
+  const { getByText } = render(<Trick trick={trick} />);
+  const trickText = getByText(/Headline 1.1.1/i);
+  expect(trickText).toBeInTheDocument();
+});
+
+
+describe('App', () => {
+  const content = `
+* Headline 1
+** Headline 1.1
+*** Headline 1.1.1
+- item 1
+- item 2
+#+BEGIN_SRC bash
+some code 1
+#+END_SRC
+*** Headline 1.1.2
+- item 1
+- item 2
+* Headline 2
+** Headline 2.1
+*** Headline 2.1.1
+- item 1
+- item 2
+- item 3
+#+BEGIN_SRC bash
+some code 2
+#+END_SRC`;
+
+
+  it('renders the 3 tricks of the content', () => {
+    const ast = parse(content);
+    const { getByText } = render(
+      <App ast={ast} />
+    );
+		screen.debug()
+    const trick_1 = getByText(/Headline 1.1.1/i);
+    const trick_2 = getByText(/Headline 1.1.2/i);
+    const trick_3 = getByText(/Headline 2.1.1/i);
+    expect(trick_1).toBeInTheDocument();
+    expect(trick_2).toBeInTheDocument();
+    expect(trick_3).toBeInTheDocument();
+  });
+})
